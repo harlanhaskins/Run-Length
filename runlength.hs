2670ddd2e908d3411162f7f9980f9cd3cad6634c
Harlan Haskins <harlan@harlanhaskins.com>
2014-05-12 16:57:51 -0400

    Reorganized code.

--- runlength.hs
+++ runlength.hs
@@ -1,17 +1,13 @@
 import Data.Digits
 import Data.List
 
-expressSingle :: Integer -> [Char]
-expressSingle x = ns !! fromInteger x
-                  where ns = ["zero", "one", "two", "three",
-                              "four", "five", "six", "seven",
-                              "eight", "nine"]
-
-runlengthList :: [Integer] -> [Integer]
-runlengthList xs = [toInteger (length xs), (xs !! 0)]
+-- Actual run-length encoding.
 
 tokenize :: Integer -> [[Integer]]
 tokenize = group . (digits 10)
+
+runlengthList :: [Integer] -> [Integer]
+runlengthList xs = [toInteger (length xs), (xs !! 0)]
 
 runlength :: Integer -> Integer
 runlength = (unDigits 10) . concat . maprunlength
@@ -21,16 +17,18 @@ runlengthLevel :: Integer -> Integer -> Integer
 runlengthLevel 0 x = x
 runlengthLevel d x = runlengthLevel (d - 1) $ runlength x
 
-expressLevel :: Integer -> Integer -> [Char]
-expressLevel d x = express $ runlengthLevel d x
+-- Expression (turning run-length encoded Integers into Strings)
 
 pairs :: [Integer] -> [(Integer, Integer)]
 pairs []        = []
 pairs (x:[])    = [(x,0)]
 pairs (x:x':xs) = (x,x') : pairs xs
 
-express :: Integer -> [Char]
-express x = intercalate " " $ map expressGroup (pairs $ (digits 10) $ runlength x)
+expressSingle :: Integer -> [Char]
+expressSingle x = ns !! fromInteger x
+                  where ns = ["zero", "one", "two", "three",
+                              "four", "five", "six", "seven",
+                              "eight", "nine"]
 
 expressGroup :: (Integer, Integer) -> String
 expressGroup xs | (fst xs == 1) = f ++ "."
@@ -38,3 +36,9 @@ expressGroup xs | (fst xs == 1) = f ++ "."
                 where f =  expressSingle (fst xs)
                         ++ " "
                         ++ expressSingle (snd xs)
+
+express :: Integer -> [Char]
+express x = intercalate " " $ map expressGroup (pairs $ (digits 10) $ runlength x)
+
+expressLevel :: Integer -> Integer -> [Char]
+expressLevel d x = express $ runlengthLevel d x
